a1ac5b346c8177cab4b8866f4b73e6b138bcc481
jgstew <james@jgstew.com>
2015-12-10 00:11:25 -0800

    Update fauxmo-automation.py

--- fauxmo-automation.py
+++ fauxmo-automation.py
@@ -1,2 +1,12 @@
 
+from fauxmo import *
+from pyviera import VieraFinder
+from wakeonlan import wol
 
+TV_WiFi_MAC = 'ff.ff.ff.ff.ff.ff'
+TV_LAN_MAC = 'ff.ff.ff.ff.ff.ff'
+
+FAUXMOS = [
+    ['tv', vieria_handler(), 45671],
+    ['sound', vizio_handler(), 45672],
+]
